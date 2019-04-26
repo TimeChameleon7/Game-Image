c8ed6d089ff1fd68c933b8ea5a63b13347c353a9
kelledar <kelledar@011-4303-S24WL.students.cisd.conroeisd.loc>
2019-04-26 07:38:09 -0500

    Changed log message for wall and object collision.

--- src/Controls.java
+++ src/Controls.java
@@ -37,7 +37,8 @@ public class Controls implements KeyListener {
             for (Point playerPoint : Main.grid.getObjects()[0].getAbsolutePoints()) {
                 for (Point point : Main.grid.getObjectsPoints()) {
                     if (playerPoint.equals(point.getLocation())) {
-                        System.out.println("Unable to translate due to object collision at " + playerPoint.getLocation());
+                        System.out.printf("Unable to translate due to object collision at (%d,%d).%n",
+                                (int)playerPoint.getX(),(int)playerPoint.getY());
                         Main.grid.getObjects()[0].setCenter(prevPoint);
                         break collisionCheck;
                     }
@@ -47,7 +48,8 @@ public class Controls implements KeyListener {
                             playerPoint.equals(new Point(0, y)) ||
                                     playerPoint.equals(new Point(Main.grid.getWidth(), y))
                     ) {
-                        System.out.println("Unable to translate due to wall collision at " + playerPoint.getLocation());
+                        System.out.printf("Unable to translate due to object wall at (%d,%d).%n",
+                                (int)playerPoint.getX(),(int)playerPoint.getY());
                         Main.grid.getObjects()[0].setCenter(prevPoint);
                         break collisionCheck;
                     }
@@ -57,7 +59,8 @@ public class Controls implements KeyListener {
                             playerPoint.equals(new Point(x, 0)) ||
                                     playerPoint.equals(new Point(x, Main.grid.getWidth()))
                     ) {
-                        System.out.println("Unable to translate due to wall collision at " + playerPoint.getLocation());
+                        System.out.printf("Unable to translate due to object wall at (%d,%d).%n",
+                                (int)playerPoint.getX(),(int)playerPoint.getY());
                         Main.grid.getObjects()[0].setCenter(prevPoint);
                         break collisionCheck;
                     }
